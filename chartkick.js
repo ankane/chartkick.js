ankane/chartkick.js

commit 63b46a5c1c2ac820767bea56ea6f35afaeb5c008

Support for named stacks w/ Highcharts & Chart.js

chartkick.js
@@ -578,7 +578,8 @@
 
             newSeries.push({
               name: series[i].name,
-              data: d
+              data: d,
+              stack: series[i].stack ? series[i].stack : null
             });
           }
           options.series = newSeries;
@@ -1227,6 +1228,7 @@
             var dataset = {
               label: s.name,
               data: rows2[i],
+              stack: s.stack ? s.stack : null,
               fill: chartType === "area",
               borderColor: color,
               backgroundColor: backgroundColor,

examples/index.html
@@ -136,6 +136,12 @@
       new Chartkick.BarChart("multiple-bar-stacked", [{name: "Series A", data: [["0",32],["1",46],["2",28],["3",21],["4",20],["5",13],["6",27]]}, {name: "Series B", data: [["0",32],["1",46],["2",28],["3",21],["4",20],["5",13],["6",27]]}], {max: 80, stacked: true});
     </script>
 
+    <h1>Multiple Series Bar Stacked &amp; Grouped (Chart.js 2.5+ or HighCharts only)</h1>
+    <div id="multiple-bar-stacked-grouped" style="height: 300px;"></div>
+    <script>
+      new Chartkick.BarChart("multiple-bar-stacked-grouped", [{name: "Series A", data: [["0",32],["1",46],["2",28],["3",21],["4",20],["5",13],["6",27]], stack: 'stack 1'}, {name: "Series B", data: [["0",32],["1",46],["2",28],["3",21],["4",20],["5",13],["6",27]], stack: 'stack 2'}, {name: "Series C", data: [["0",12],["1",16],["2",18],["3",11],["4",10],["5",3],["6",17]], stack: 'stack 2'}], {max: 80, stacked: true});
+    </script>
+
     <h1>Multiple Series Column Stacked</h1>
     <div id="multiple-column-stack" style="height: 300px;"></div>
     <script>

commit b865bf06bf35fd04e59914932974658b454f48f1

Fixed stacking issue introduced in #83

chartkick.js
@@ -569,18 +569,22 @@
 
           options.xAxis.categories = categories;
 
-          var newSeries = [];
+          var newSeries = [], d2;
           for (i = 0; i < series.length; i++) {
             d = [];
             for (j = 0; j < categories.length; j++) {
               d.push(rows[categories[j]][i] || 0);
             }
 
-            newSeries.push({
+            d2 = {
               name: series[i].name,
-              data: d,
-              stack: series[i].stack ? series[i].stack : null
-            });
+              data: d
+            }
+            if (series[i].stack) {
+              d2.stack = series[i].stack;
+            }
+
+            newSeries.push(d2);
           }
           options.series = newSeries;
 
@@ -1231,13 +1235,16 @@
             var dataset = {
               label: s.name,
               data: rows2[i],
-              stack: s.stack ? s.stack : null,
               fill: chartType === "area",
               borderColor: color,
               backgroundColor: backgroundColor,
               pointBackgroundColor: color,
               borderWidth: 2
             };
+
+            if (s.stack) {
+              dataset.stack = s.stack;
+            }
 
             if (chart.options.curve === false) {
               dataset.lineTension = 0;