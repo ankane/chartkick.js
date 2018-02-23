057692b79f4a63efe58eb7dd7a7e01966423b584
Andrew <acekane1@gmail.com>
2018-02-22 20:45:47 -0800

    Better repo structure

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require("webpack")
+var path = require("path")
+
+module.exports = {
+  entry: {
+    "chartkick": "./src/chartkick.js",
+    "chartkick.min": "./src/chartkick.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ]
+}
