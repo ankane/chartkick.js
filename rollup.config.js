3544e1526118a540cddda0ba246838fe308e13c4
Andrew Kane <andrew@chartkick.com>
2019-05-14 17:36:45 -0700

    Updated rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import buble from "rollup-plugin-buble";
 import commonjs from "rollup-plugin-commonjs";
 import pkg from "./package.json";
 import resolve from "rollup-plugin-node-resolve";
-import uglify from "rollup-plugin-uglify";
+import { uglify } from "rollup-plugin-uglify";
 
 const input = "src/index.js";
 const outputName = "Chartkick";
