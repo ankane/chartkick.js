945c9ed70c70d7fda572fb7073e55259fb19ad0c
Andrew Kane <andrew@chartkick.com>
2020-03-18 19:30:07 -0700

    Updated plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import buble from "rollup-plugin-buble";
-import commonjs from "rollup-plugin-commonjs";
+import buble from "@rollup/plugin-buble";
+import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import { uglify } from "rollup-plugin-uglify";
 
 const input = "src/index.js";
